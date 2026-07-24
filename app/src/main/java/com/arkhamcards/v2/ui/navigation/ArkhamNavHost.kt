dbdc7683db95b3e455b4b01fbd258e9c03ee6591
Evgeny727 <genyakarelin23@gmail.com>
2026-07-24 18:06:48 +0300

    Made keyboard be on top

--- app/src/main/java/com/arkhamcards/v2/ui/navigation/ArkhamNavHost.kt
+++ app/src/main/java/com/arkhamcards/v2/ui/navigation/ArkhamNavHost.kt
@@ -19,12 +19,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
@@ -106,7 +110,7 @@ fun ArkhamNavHost(viewModel: AppViewModel) {
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .safeDrawingPadding(),
+            .windowInsetsPadding(WindowInsets.statusBars.union(WindowInsets.navigationBars)),
         containerColor = CustomTheme.colors.background,
         topBar = {
             ArkhamTopAppBar(
