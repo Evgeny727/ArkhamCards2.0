44b9d4d440019819f0d49291d9e7fea61751144b
Evgeny727 <genyakarelin23@gmail.com>
2026-08-07 11:38:37 +0300

    Handle cases when image not loaded

--- app/src/main/java/com/arkhamcards/v2/ui/cards/components/details/CardDetailsClickableThumbnail.kt
+++ app/src/main/java/com/arkhamcards/v2/ui/cards/components/details/CardDetailsClickableThumbnail.kt
@@ -7,6 +7,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,43 +42,50 @@ fun CardDetailsClickableThumbnail(
     faction2: Faction?,
     modifier: Modifier = Modifier
 ) {
-    if (thumbnailUrl != null) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(thumbnailUrl)
-                .build(),
-            modifier = modifier
-                .size(108.dp)
-                .clip(CustomTheme.shapes.medium)
-                .clickable(enabled = imageUrl != null) {
-                    /*TODO:open full sized image in dialog*/
-                },
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-        )
-    } else {
-        Box(
-            modifier = modifier
-                .size(108.dp)
-                .clip(CustomTheme.shapes.medium)
-                .background(CustomTheme.colors.divider)
-                .border(1.dp, CustomTheme.colors.darkText, CustomTheme.shapes.medium)
-                .clickable(enabled = imageUrl != null) {
-                    /*TODO:open full sized image in dialog*/
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            val icon = if (encounterCode != null) {
-                PackIcon.fromPackCode(encounterCode)
-            } else {
-                factionIcon(faction, faction2, subType)
-            }
+    var hidePlaceholder by remember { mutableStateOf(false) }
 
-            Text(
-                text = icon.glyph,
-                fontFamily = icon.fontFamily,
-                fontSize = 48.sp,
-                color = CustomTheme.colors.lightText
+    Box(contentAlignment = Alignment.Center) {
+        if (!hidePlaceholder) {
+            Box(
+                modifier = modifier
+                    .size(108.dp)
+                    .clip(CustomTheme.shapes.medium)
+                    .background(CustomTheme.colors.divider)
+                    .border(1.dp, CustomTheme.colors.darkText, CustomTheme.shapes.medium)
+                    .clickable(enabled = imageUrl != null) {
+                        /*TODO:open full sized image in dialog*/
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                val icon = if (encounterCode != null) {
+                    PackIcon.fromPackCode(encounterCode)
+                } else {
+                    factionIcon(faction, faction2, subType)
+                }
+
+                Text(
+                    text = icon.glyph,
+                    fontFamily = icon.fontFamily,
+                    fontSize = 48.sp,
+                    color = CustomTheme.colors.lightText
+                )
+            }
+        }
+
+        if (thumbnailUrl != null) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(thumbnailUrl)
+                    .build(),
+                modifier = modifier
+                    .size(108.dp)
+                    .clip(CustomTheme.shapes.medium)
+                    .clickable(enabled = imageUrl != null) {
+                        /*TODO:open full sized image in dialog*/
+                    },
+                onSuccess = { hidePlaceholder = true },
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
             )
         }
     }
