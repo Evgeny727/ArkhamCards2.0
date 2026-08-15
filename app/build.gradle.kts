3912c6f49283ea6a815215606f0ce5785823a92b
Evgeny727 <genyakarelin23@gmail.com>
2026-08-15 14:18:50 +0300

    Set version for closed testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,9 @@ android {
     defaultConfig {
         applicationId = "com.arkhamcompanion"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
-        versionName = "0.0.1-alpha"
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
